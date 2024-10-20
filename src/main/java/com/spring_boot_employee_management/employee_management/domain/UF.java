3414a40cdeb699bc8fd6328377b57812e9953b05
Dady francisco mendes <dev_dady@outlook.com>
2024-10-20 14:02:27 -0300

    criando entidade endereco

---
+++ src/main/java/com/spring_boot_employee_management/employee_management/domain/UF.java
@@ -0,0 +1,54 @@
+package com.spring_boot_employee_management.employee_management.domain;
+
+public enum UF {
+    AC("AC", "Acre"),
+    AL("AL", "Alagoas"),
+    AM("AM", "Amazonas"),
+    BA("BA", "Bahia"),
+    CE("CE", "Ceará"),
+    DF("DF", "Distrito Federal"),
+    ES("ES", "Espírito Santo"),
+    GO("GO", "Goiás"),
+    MA("MA", "Maranhão"),
+    MT("MT", "Mato Grosso"),
+    MS("MS", "Mato Grosso do Sul"),
+    MG("MG", "Minas Gerais"),
+    PA("PA", "Pará"),
+    PB("PB", "Paraíba"),
+    PR("PR", "Paraná"),
+    PE("PE", "Pernambuco"),
+    PI("PI", "Piauí"),
+    RJ("RJ", "Rio de Janeiro"),
+    RN("RN", "Rio Grande do Norte"),
+    RS("RS", "Rio Grande do Sul"),
+    RO("RO", "Rondônia"),
+    RR("RR", "Roraima"),
+    SC("SC", "Santa Catarina"),
+    SP("SP", "São Paulo"),
+    SE("SE", "Sergipe"),
+    TO("TO", "Tocantins");
+
+    private  String sigla;
+    private String description;
+
+    UF(String sigla, String description) {
+        this.sigla = sigla;
+        this.description = description;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

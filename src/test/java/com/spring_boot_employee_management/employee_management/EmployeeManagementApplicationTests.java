25e1dd44164be14503ec986f088c4e9ecc28a6fb
Dady francisco mendes <dev_dady@outlook.com>
2024-10-20 00:46:50 -0300

    fist commit

---
+++ src/test/java/com/spring_boot_employee_management/employee_management/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_employee_management.employee_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

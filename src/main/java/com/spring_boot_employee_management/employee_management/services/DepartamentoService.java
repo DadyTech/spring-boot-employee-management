1a90e7ec7f69432e11da40cbd33006a1afe26939
Dady francisco mendes <dev_dady@outlook.com>
2024-10-22 23:04:19 -0300

    Finalizando os Services

--- src/main/java/com/spring_boot_employee_management/employee_management/services/DepartamentoService.java
+++ src/main/java/com/spring_boot_employee_management/employee_management/services/DepartamentoService.java
@@ -1,4 +1,16 @@
 package com.spring_boot_employee_management.employee_management.services;
 
+import com.spring_boot_employee_management.employee_management.domain.Cargo;
+import com.spring_boot_employee_management.employee_management.domain.Departamento;
+
+import java.util.List;
+
 public interface DepartamentoService {
+
+
+    void salvar(Departamento departamento);
+    void editar(Departamento departamento);
+    void excluir(Long id);
+    Departamento buscarPorId(Long id);
+    List<Departamento> buscarTodos();
 }
--- src/main/java/com/spring_boot_employee_management/employee_management/services/FuncionarioService.java
+++ src/main/java/com/spring_boot_employee_management/employee_management/services/FuncionarioService.java
@@ -1,4 +1,15 @@
 package com.spring_boot_employee_management.employee_management.services;
 
+import com.spring_boot_employee_management.employee_management.domain.Cargo;
+import com.spring_boot_employee_management.employee_management.domain.Funcionario;
+
+import java.util.List;
+
 public interface FuncionarioService {
+
+    void salvar(Funcionario funcionario);
+    void editar(Funcionario funcionario);
+    void excluir(Long id);
+    Funcionario buscarPorId(Long id);
+    List<Funcionario> buscarTodos();
 }
